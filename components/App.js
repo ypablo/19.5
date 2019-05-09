4b9bf33c57872d1325140e41462a0fb7a221cc1a
ypablo <yfedora@gmail.com>
2019-05-09 01:00:48 +0100

    19.5 Promises

--- components/App.js
+++ components/App.js
@@ -63,40 +63,42 @@ App = React.createClass({
       loading: true // 2.
     });
     this.getGif(
+
       searchingText
-        .then((gif) => {
-          // 3.
-          this.setState({
-            // 4
-            loading: false, // a
-            gif: gif, // b
-            searchingText: searchingText // c
-          });
-        }.bind(this)
-    )
+        .then(
+          function (gif) {
+            // 3.
+            this.setState({
+              // 4
+              loading: false, // a
+              gif: gif, // b
+              searchingText: searchingText // c
+            });
+          }.bind(this)
+        )
     );
   },
 
-render: function () {
-  var styles = {
-    margin: "0 auto",
-    textAlign: "center",
-    width: "90%"
-  };
-  return (
-    <div style={styles}>
-      <h1>Wyszukiwarka GIFow!</h1>
-      <p>
-        Znajdź gifa na <a href="http://giphy.com">giphy</a>. Naciskaj enter,
-        aby pobrać kolejne gify.
+  render: function () {
+    var styles = {
+      margin: "0 auto",
+      textAlign: "center",
+      width: "90%"
+    };
+    return (
+      <div style={styles}>
+        <h1>Wyszukiwarka GIFow!</h1>
+        <p>
+          Znajdź gifa na <a href="http://giphy.com">giphy</a>. Naciskaj enter,
+          aby pobrać kolejne gify.
         </p>
-      <Search onSearch={this.handleSearch} />
-      <Gif
-        loading={this.state.loading}
-        url={this.state.gif.url}
-        sourceUrl={this.state.gif.sourceUrl}
-      />
-    </div>
-  );
-}
+        <Search onSearch={this.handleSearch} />
+        <Gif
+          loading={this.state.loading}
+          url={this.state.gif.url}
+          sourceUrl={this.state.gif.sourceUrl}
+        />
+      </div>
+    );
+  }
 });
