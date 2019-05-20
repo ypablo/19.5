5e513ce647b413583a3f5b8acd523da4efa71fd6
ypablo <yfedora@gmail.com>
2019-05-21 00:52:35 +0100

    Add promise

--- components/App.js
+++ components/App.js
@@ -8,37 +8,7 @@ App = React.createClass({
       gif: {}
     };
   },
-/*
-  getGif: function (searchingText) {
-    return new Promise((resolve, reject) => {
-
-      var GIPHY_API_URL = "https://api.giphy.com";
-      var GIPHY_PUB_KEY = "MYQbFdMBoJ1XLKdR14UkuhuGuezHerle";
-      // 1.
-      var url = GIPHY_API_URL + "/v1/gifs/random?api_key=" + GIPHY_PUB_KEY + "&tag=" + searchingText; // 2.
-      var xhr = new XMLHttpRequest(); // 3.
-      xhr.open("GET", url);
-      
-        xhr.onload = function () {  
-          if (xhr.status === 200) {
-            
-            var data = JSON.parse(xhr.responseText).data; // 4.
-            var gif = {
-              // 5.
-              url: data.fixed_width_downsampled_url,
-              sourceUrl: data.url
-            };
-            
-          }
-        };
-      
-      xhr.send();
-      
-    }); 
-  },
-*/
   
-  //getGif: function(searchingText, callback) {
   getGif: function(searchingText) {  
     return new Promise((resolve, reject) => {
     var GIPHY_API_URL = "https://api.giphy.com";
@@ -54,7 +24,6 @@ App = React.createClass({
           url: data.fixed_width_downsampled_url,
           sourceUrl: data.url
         };
-        //callback(gif); // 6.
         resolve(gif);
       }
     };
